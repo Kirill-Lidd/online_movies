8201db35bc6e0187b41ad9d5c1ec6cdf982c93c6
Kirill <108875800+Kirill-Lidd@users.noreply.github.com>
2022-08-15 12:55:06 +0300

    add seeders

--- app/Http/Controllers/Admin/Film/DestroyController.php
+++ app/Http/Controllers/Admin/Film/DestroyController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Admin\Film;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Models\Film;
 
 class DestroyController extends Controller
 {
     public function __invoke(Film $film)
-    {
+    {	
     	$film->delete();
     	return redirect()->back();
     }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::insert([
+        	'name' => 'admin',
+        	'email' => 'admin@ad.min',
+        	'password' => Hash::make('123'),
+        	'role' => '1',
+        ]);
+    }
+}
